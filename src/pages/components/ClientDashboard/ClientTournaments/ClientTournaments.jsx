1bad2c2957d49c5ccb1ff11cdf5a9f72e7a70dbc
crdevelopers506 <crdevelopers506@gmail.com>
2023-08-26 16:34:41 -0600

    side bar moved to navigation component

--- src/pages/components/ClientDashboard/ClientTournaments/ClientTournaments.jsx
+++ src/pages/components/ClientDashboard/ClientTournaments/ClientTournaments.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { ClientNavigation } from '../ClientNavigation/ClientNavigation'
 
@@ -5,6 +6,7 @@ export const ClientTournaments = () => {
   return (
     <div>
         <ClientNavigation/>
+        test
     </div>
   )
 }
