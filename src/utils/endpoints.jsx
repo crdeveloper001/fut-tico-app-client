8fd809a1c4a9b7143b2459afb2b8cfe2c4cae24f
crdevelopers506 <crdevelopers506@gmail.com>
2023-09-01 17:26:14 -0600

    .

--- src/utils/endpoints.jsx
+++ src/utils/endpoints.jsx
@@ -1,6 +1,6 @@
 //API BASE ENDPOINTS
-export const BASE_URL = 'http://localhost:8080';
-//export const BASE_URL = "https://app-a51f3f31-e875-43b6-b74d-ef19d9045215.cleverapps.io";
+//export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = "https://app-a51f3f31-e875-43b6-b74d-ef19d9045215.cleverapps.io";
 
 //AUTHENTICATION
 export const api_Authentication = "/api/v1/Authorization/AuthenticateProfile"
