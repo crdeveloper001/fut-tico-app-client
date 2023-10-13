fa8ec61c72d948e8af98f860807b0b7f0ad14c01
crdevelopers506 <crdevelopers506@gmail.com>
2023-10-13 11:59:40 -0600

    redux commented for now

--- src/pages/store/reducers/navigation.js
+++ src/pages/store/reducers/navigation.js
@@ -1,22 +1,13 @@
-import { getCurrentFields } from "../actions/Fields/actions";
+// import { getCurrentFields } from "../actions/Fields/actions";
 
-const initialState = {
-  client_Navigation: 0,
-  administrator_Navigation: 0,
-  general_Navigation: 0,
-};
+// const initialState = {
+//   client_Navigation: 0,
+//   administrator_Navigation: 0,
+//   general_Navigation: 0,
+// };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ACTIVE_ADMIN_NAV:
-      return { ...state, administratorNavigation: 1 };
-    case ACTIVE_CLIENT_NAV:
-      return { ...state, clientNavigation: 1 };
-    case ACTIVE_GENERAL_NAV:
-      return { ...state, generalNavigation: 1 };
-    default:
-      return state;
-  }
-};
+// const reducer = (state = initialState, action) => {
+  
+// };
 
-export default reducer;
+// export default reducer;
