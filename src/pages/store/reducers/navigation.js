eaeb79c51fa493e08eea6cebf4b5d74db4af523d
crdevelopers506 <crdevelopers506@gmail.com>
2023-10-13 12:00:51 -0600

    .

--- src/pages/store/reducers/navigation.js
+++ src/pages/store/reducers/navigation.js
@@ -1,13 +1,13 @@
-// import { getCurrentFields } from "../actions/Fields/actions";
+import { getCurrentFields } from "../actions/Fields/actions";
 
-// const initialState = {
-//   client_Navigation: 0,
-//   administrator_Navigation: 0,
-//   general_Navigation: 0,
-// };
+const initialState = {
+  client_Navigation: 0,
+  administrator_Navigation: 0,
+  general_Navigation: 0,
+};
 
-// const reducer = (state = initialState, action) => {
+const reducer = () => {
   
-// };
+};
 
-// export default reducer;
+export default reducer;
